b7b4fe4c1bec45bcc2eaa1fa274fe8728636a359
windoctober <2908709191@qq.com>
2023-05-09 19:25:01 +0800

    2023年 05月 09日 星期二 19:25:01 CST

---
+++ Stingx/CMakeFiles/lstingX.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "../lstingX"
+  "../lstingX.pdb"
+  "CMakeFiles/lstingX.dir/Clump.cpp.o"
+  "CMakeFiles/lstingX.dir/Context.cpp.o"
+  "CMakeFiles/lstingX.dir/DisequalityStore.cpp.o"
+  "CMakeFiles/lstingX.dir/DualInvariantMap.cpp.o"
+  "CMakeFiles/lstingX.dir/DualTransitionRelation.cpp.o"
+  "CMakeFiles/lstingX.dir/Elimination.cpp.o"
+  "CMakeFiles/lstingX.dir/Expression.cpp.o"
+  "CMakeFiles/lstingX.dir/ExpressionStore.cpp.o"
+  "CMakeFiles/lstingX.dir/InvariantMap.cpp.o"
+  "CMakeFiles/lstingX.dir/LinExpr.cpp.o"
+  "CMakeFiles/lstingX.dir/LinTransform.cpp.o"
+  "CMakeFiles/lstingX.dir/Location.cpp.o"
+  "CMakeFiles/lstingX.dir/Macro.cpp.o"
+  "CMakeFiles/lstingX.dir/MatrixStore.cpp.o"
+  "CMakeFiles/lstingX.dir/PolyStore.cpp.o"
+  "CMakeFiles/lstingX.dir/PolyUtils.cpp.o"
+  "CMakeFiles/lstingX.dir/Propagation.cpp.o"
+  "CMakeFiles/lstingX.dir/Rational.cpp.o"
+  "CMakeFiles/lstingX.dir/SparseLinExpr.cpp.o"
+  "CMakeFiles/lstingX.dir/SparseLinTransform.cpp.o"
+  "CMakeFiles/lstingX.dir/System.cpp.o"
+  "CMakeFiles/lstingX.dir/Timer.cpp.o"
+  "CMakeFiles/lstingX.dir/ToolUtils.cpp.o"
+  "CMakeFiles/lstingX.dir/TransitionRelation.cpp.o"
+  "CMakeFiles/lstingX.dir/Tree.cpp.o"
+  "CMakeFiles/lstingX.dir/funcs.cpp.o"
+  "CMakeFiles/lstingX.dir/lstingx.cpp.o"
+  "CMakeFiles/lstingX.dir/var-info.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lstingX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Stingx/CMakeFiles/lstingx.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "liblstingx.a"
+)
