093277f25535f1379564243504a64b260073eee1
windoctober <2908709191@qq.com>
2023-05-09 19:25:58 +0800

    2023年 05月 09日 星期二 19:25:58 CST

---
+++ upload.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Check if git is installed
+if ! command -v git >/dev/null 2>&1; then
+    echo "Error: git not found. Please ensure git is installed and in your PATH."
+    exit 1
+fi
+
+# Run 'git add .'
+echo "Adding all files to the staging area..."
+git add . || { echo "Error: Unable to add files to the staging area."; exit 1; }
+
+# Get the current system time as the commit message
+commit_message=$(date)
+
+# Run 'git commit'
+echo "Creating a new commit..."
+git commit -m "${commit_message}" || { echo "Error: Unable to create a new commit."; exit 1; }
+
+# Run 'git push'
+echo "Pushing changes to GitHub..."
+git push || { echo "Error: Unable to push changes to GitHub."; exit 1; }
+
+echo "One-click upload script completed!"
